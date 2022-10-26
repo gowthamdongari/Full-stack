dd057c813483d379b781b91b42128ee16836d916
gowthamdongari <gowthamdnr@gmail.com>
2022-10-26 13:34:14 +0530

    javascript updates

---
+++ JavaScript/01. Intro to JS & variables/01. intro to Dynamic web applications.js
@@ -0,0 +1,17 @@
+console.log("Hello world");
+
+let message;
+//let is used to create  variables and assign a value//
+message = "Hello Gowtham"; 
+
+console.log(message);
+
+let msg = "Hai Gowtham";
+
+console.log(msg)
+
+let nothing;
+
+console.log(nothing);
+
+console.log(123);
