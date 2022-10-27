e03b8cbfc6b093fe3fa415fd5bcbfd2b873c66e0
gowthamdongari <gowthamdnr@gmail.com>
2022-10-27 22:17:49 +0530

    updATE

---
+++ JavaScript/02. DOM & event Fundamentals/01.DOM&EVENT.js
@@ -0,0 +1,6 @@
+
+
+function manipulateStyles() {
+    document.getElementById("headingElement").textContent = "4.O Technologies";
+    document.getElementById("headingElement").style.color = "blue";
+  }
