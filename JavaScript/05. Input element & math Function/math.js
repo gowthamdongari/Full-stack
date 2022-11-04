144c6bc7ef3ebad629c56acaea8295915845dd3e
gowthamdongari <gowthamdnr@gmail.com>
2022-11-04 12:52:15 +0530

    js math function

---
+++ JavaScript/05. Input element & math Function/math.js
@@ -0,0 +1,10 @@
+console.log(Math.random()*100);
+
+console.log(Math.ceil(9.55));
+
+console.log(Math.ceil(99.55));
+//next number is given in if we use math ceil //
+
+let randomNumber = Math.random()*100;
+console.log(randomNumber);
+console.log(Math.ceil(randomNumber));
