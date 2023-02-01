fbceba09460ada98ea0bdac595dfe57ba4a54913
gowthamdongari <gowthamdnr@gmail.com>
2023-02-01 09:10:14 +0530

    tip calculator

---
+++ JavaScript/12. Tip Calculator/Tip Calculator.js
@@ -0,0 +1,27 @@
+let billAmountInput = document.getElementById("billAmount");
+let percentageTipInput = document.getElementById("percentageTip");
+let tipAmountInput = document.getElementById("tipAmount");
+let totalInput = document.getElementById("totalAmount");
+let errorMessageElement = document.getElementById("errorMessage");
+let errorMessage = "Please Enter a valid Input.";
+
+function calculateTip() {
+    let billAmountInputValue = billAmountInput.value;
+    let percentageTipInputValue = percentageTipInput.value;
+
+    if (billAmountInputValue === ""){
+        errorMessageElement.textContent = errorMessage;
+    } else if (percentageTipInputValue === ""){
+        errorMessageElement.textContent = errorMessage;
+    } else {
+        errorMessageElement.textContent = "";
+        let billAmount = parseInt(billAmountInputValue);
+        let percentageTip = parseInt(percentageTipInputValue);
+        
+        let calculateTip = (percentageTip / 100)*billAmount;
+        let calculateTotal = billAmount + calculateTip;
+
+        tipAmountInput.value = calculateTip;
+        totalInput.value = calculateTotal;
+    }
+}
