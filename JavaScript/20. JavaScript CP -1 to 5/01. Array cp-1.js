6259e124072f95c270f1350d5ebce0c61790fb0a
gowthamdongari <gowthamdnr@gmail.com>
2023-02-06 06:42:41 +0530

    Arrays js

---
+++ JavaScript/20. JavaScript CP -1 to 5/01. Array cp-1.js
@@ -0,0 +1,7 @@
+/*
+ * Write a js program to log an array with the given values.
+'orange', '25', '100', 'true', '33', '58'
+ */
+let myArray = [ 'Orange', 25, 100, true, 33.58 ];
+
+console.log(myArray);
