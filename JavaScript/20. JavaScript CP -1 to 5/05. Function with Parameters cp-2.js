2c31851eb1af22717880cf9d4838cd7e3663836c
gowthamdongari <gowthamdnr@gmail.com>
2023-02-11 12:13:20 +0530

    js

---
+++ JavaScript/20. JavaScript CP -1 to 5/05. Function with Parameters cp-2.js
@@ -0,0 +1,35 @@
+"use strict";
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", (inputStdin) => {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", (_) => {
+  inputString = inputString
+    .trim()
+    .split("\n")
+    .map((str) => str.trim());
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+function main() {
+  // firstInteger
+  let firstInteger = parseInt(readLine());
+  // secondInteger
+  let secondInteger = parseInt(readLine());
+  
+  let avg_of_two_nums = (firstInteger + secondInteger)/2;
+console.log(avg_of_two_nums);
+
+}
