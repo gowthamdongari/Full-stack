3a1b0f774541ee5078873f12906331d42173ac70
gowthamdongari <gowthamdnr@gmail.com>
2023-02-17 07:37:15 +0530

    eligibility to vote

---
+++ JavaScript/002. Arrays & Objects/04. JavaScript CP -1 to 5/03. Eligibility to vote cp-5.js
@@ -0,0 +1,34 @@
+"use strict";
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", (inputStdin) => {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", (_) => {
+  inputString = inputString
+    .trim()
+    .split("\n")
+    .map((str) => str.trim());
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+function main() {
+  // arrayOfPersons
+  let arrayOfPersons = JSON.parse(readLine().replace(/'/g, '"'));
+for (let item of arrayOfPersons){
+    if (item['age']>=18) {
+        console.log(item["name"]);
+    }
+}
+}
