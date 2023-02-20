c41b6e54416aaabb4084c53393d8fcd9ef664906
gowthamdongari <gowthamdnr@gmail.com>
2023-02-20 06:21:17 +0530

    log an object

---
+++ JavaScript/002. Arrays & Objects/04. JavaScript CP -1 to 5/01. Create & Log an Object cp-3.js
@@ -0,0 +1,8 @@
+let vehicle = {
+    name : "Mercedes Benz",
+    model : "C-Class",
+    manufacturer : "Daimler AG company",
+    length : "4686mm",
+    'cargo capacity' : '12.6 cubic feet'
+}
+console.log(vehicle);
