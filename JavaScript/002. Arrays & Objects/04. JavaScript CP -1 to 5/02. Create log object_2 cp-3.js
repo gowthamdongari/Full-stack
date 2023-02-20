557700cd8ecd0f5056d041b474a9d86680449dcf
gowthamdongari <gowthamdnr@gmail.com>
2023-02-20 06:21:46 +0530

    object

---
+++ JavaScript/002. Arrays & Objects/04. JavaScript CP -1 to 5/02. Create log object_2 cp-3.js
@@ -0,0 +1,12 @@
+let movie = {
+    title : "Baahubali",
+    director : "Rajamouli",
+    producers : [ 'Sobhu', 'Raghavendra', 'Prasad'],
+    'lead actors with their roles' : {
+        prabhas : "Baahubali",
+        anushka : 'Devasena',
+        rana : 'Bhallaladeva',
+        tamannah : 'Avanthika'
+    }
+}
+console.log(movie);
